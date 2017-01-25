c65e93a7a8b7649e36ef55545d996d973003b419
Vishal Patel <Vish@Vishals-MacBook-Pro-2.local>
2017-01-25 19:33:28 +0000

    implented geolocation babygit add .

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,35 @@
-angular.module('tshirt-weather.controllers', ['ionic'])
+angular.module('tshirt-weather.controllers', ['ionic', 'ngCordova'])
 
-.constant('eb049bc3aa34712e2762d3ba63e9c93d', 'eb049bc3aa34712e2762d3ba63e9c93d')
-  .controller('WeatherCtrl', function($scope, $state, Weather, DataStore) {
-    //read default settings into scope
-    console.log('inside weather');
-    $scope.city = DataStore.city;
-    var latitude = DataStore.latitude;
-    var longitude = DataStore.longitude;
+    .constant('eb049bc3aa34712e2762d3ba63e9c93d', 'eb049bc3aa34712e2762d3ba63e9c93d')
+    .controller('WeatherCtrl', function($scope, $cordovaGeolocation, $state, Weather, DataStore) {
 
-    //call getCurrentWeather method in factory ‘Weather’
-    Weather.getCurrentWeather(latitude, longitude)
-      .then(function(resp) {
-        $scope.current = resp.data;
-        console.log('GOT CURRENT', $scope.current);
-        //debugger;
-      }, function(error) {
-        alert('Unable to get current conditions');
-        console.error(error);
-      });
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 60000
+        };
 
-  });
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position) {
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            $scope.geoLat = lat;
+            $scope.geoLong = long;
+        });
+
+        //read default settings into scope
+        console.log('inside weather');
+        $scope.city = DataStore.city;
+        var latitude = DataStore.latitude;
+        var longitude = DataStore.longitude;
+
+        //call getCurrentWeather method in factory ‘Weather’
+        Weather.getCurrentWeather(latitude, longitude)
+            .then(function(resp) {
+                $scope.current = resp.data;
+                console.log('GOT CURRENT', $scope.current);
+                //debugger;
+            }, function(error) {
+                alert('Unable to get current conditions');
+                console.error(error);
+            });
+    });

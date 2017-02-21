543e9523a76ab6721c931511516b756b034159a9
Vishal Patel <Vish@vishals-mbp-2.home>
2017-02-21 18:43:07 +0000

    warning huge commit: latest - now have a testing homepage displaying various weather

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,7 @@ angular.module('tshirt-weather.controllers', ['ionic', 'ngCordova'])
                     .then(function(resp) {
                         $scope.current = resp.data;
                         console.log('GOT CURRENT', $scope.current);
-                        console.log(((new Date()).getTime() - $scope.startTime)/1000);
+                        console.log(((new Date()).getTime() - $scope.startTime) / 1000);
                         return true
                         //debugger;
                     }, function(error) {
@@ -36,10 +36,23 @@ angular.module('tshirt-weather.controllers', ['ionic', 'ngCordova'])
                 //         }
                 //     }
                 // })
-
-
             });
         $scope.city = DataStore.city;
+
+        $scope.temperatureTest = function(type) {
+            if ($scope.current == null) {
+                setTimeout(function() {
+                    $scope.temperatureTest();
+                }, 500); // Try to submit form after timeout
+            } else {
+                if (type == 'precipProbability') {
+                  var averageTemp = HourlyCalculator.getMax($scope.current, type, 5);
+                } else {
+                    var averageTemp = HourlyCalculator.getAverage($scope.current, type, 5);
+                }
+            }
+            return averageTemp;
+        }
 
         $scope.isTshirtWeather = function() {
             if ($scope.current == null) {
@@ -49,9 +62,52 @@ angular.module('tshirt-weather.controllers', ['ionic', 'ngCordova'])
             } else {
                 var temp = HourlyCalculator.getAverage($scope.current, 'temperature', 5);
                 var cloud = HourlyCalculator.getMax($scope.current, 'cloudCover', 5);
-                var boolean = temp > 18 ? true : false;
+                var rainprop = HourlyCalculator.getAverage($scope.current, 'precipProbability', 5);
+                var boolean = temp > 24 ? true : false;
+                // if (temp > 18 && cloud == 0) {
+                //     boolean = true;
+                // } else if (temp > 20 && cloud < 0.3) {
+                //     boolean = true;
+                // }
+                if (rainprop < 0.2) {
+                    boolean = (temp >= (6 * cloud) + 18) ? true : false; //equation subject to change
+                }
             }
             return boolean
+        }
+
+        $scope.torsoOption = function() {
+            var torso = "tshirt"
+            if ($scope.current == null) {
+                setTimeout(function() {
+                    $scope.torsoOption();
+                }, 500); // Try to submit form after timeout
+            } else {
+                var temp = HourlyCalculator.getAverage($scope.current, 'temperature', 5);
+                var apaTemp = HourlyCalculator.getAverage($scope.current, 'apparentTemperature', 5);
+                var cloud = HourlyCalculator.getMax($scope.current, 'cloudCover', 5);
+                var rainprop = HourlyCalculator.getAverage($scope.current, 'precipProbability', 5);
+                var raini = HourlyCalculator.getMax($scope.current, 'precipIntensity', 5);
+                if (temp >= (6 * cloud) + 18 && rainprop < 0.2) {
+                    torso = "tshirt"
+                } else if (temp >= (6 * cloud) + 15 && apaTemp > 20) {
+                    torso = "longsleeve"
+                } else if (temp >= (6 * cloud) + 15 && apaTemp < 20) {
+                    torso = "light-jacket"
+                } else if (temp >= (6 * cloud) + 10 && apaTemp > 12) {
+                    torso = "jacket"
+                } else if (temp >= (6 * cloud) + 10 && apaTemp < 12) {
+                    torso = "hoodie"
+                }
+                // else if (rainprop > 0.2) {
+                //     torso = "coat"
+                // }
+                else {
+                    torso = "layers"
+                }
+            }
+            return torso
+
         }
 
     });
